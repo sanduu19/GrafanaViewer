e5be78980f8cb67c237aabb0e41cf3b9eba99f1e
IT21054822 <it21054822@my.sliit.lk>
2024-02-19 16:53:20 +0530

    styling

--- frontend/frontend-template/src/components/Sidebar/SideBar.tsx
+++ frontend/frontend-template/src/components/Sidebar/SideBar.tsx
@@ -4,6 +4,7 @@ import './SideBar.css';
 import Dashboard1 from '../Dashboards/Dashboard1'
 import bg2 from '../Assets/bg1008.jpg'
 import logo111 from '../Assets/Logo111.svg'
+import barc from '../Assets/bar-chart_3589813.png'
 
 const SideBar: React.FC = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
@@ -21,18 +22,18 @@ const SideBar: React.FC = () => {
       <div className="sidebar-elements">
         <img className='logo111' src={logo111} alt="" />
         <div className="element">
-          <button className='bn' onClick={() => { setMenu("1") }}>Power Consumption{menu === "1" ? <hr /> : <></>} </button>
+          <button className='bn' onClick={() => { setMenu("1") }}> <img src={barc} alt="" style={{width:'30px'}}/> Power Consumption{menu === "1" ? <hr /> : <></>} </button>
         </div>
-        <div className="element with-dropdown">
-          <button className='bn' >Unit/Costs</button>
-          <div className="dropdown">
+        <div className="element with-dropdown" >
+          <button className='bn' style={{marginTop:'-70px'}}>Unit/Costs</button>
+          <div className="dropdown" style={{marginTop:'-20px', marginLeft:'13px'}}>
             <p onClick={() => { setMenu("2") }}>Unit {menu === "3" ? <hr /> : <></>}</p>
             <p onClick={() => { setMenu("3") }}>Costs {menu === "4" ? <hr /> : <></>}</p>
           </div>
         </div>
         <div className="element with-dropdown">
-          <button className='bn' >Donut on Unit/Costs</button>
-          <div className="dropdown">
+          <button className='bn' style={{marginTop:'-16px'}} >Donut on Unit/Costs</button>
+          <div className="dropdown" style={{marginTop:'-0px', marginLeft:'13px'}}>
             <p onClick={() => { setMenu("4") }}>Unit {menu === "6" ? <hr /> : <></>}</p>
             <p onClick={() => { setMenu("5") }}>Cost {menu === "7" ? <hr /> : <></>}</p>
           </div>
